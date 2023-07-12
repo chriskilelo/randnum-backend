ea2775fbd1d0e1358aa8ee8eeee677bdfff498a6
chriskilelo <chriskilelo@gmail.com>
2023-07-12 12:47:34 +0300

    Implemented routing for Random Number CRUD operations.

--- routes/randomNumberRoutes.js
+++ routes/randomNumberRoutes.js
@@ -1,5 +1,16 @@
 /**
  * TO GOD BE ALL THE GLORY
- * 12-JUL-2023
+ * 12-JUL-2023 1245
  * This file will contain all the routes for Random Number generation
  */
+const express = require('express')
+const router = express.Router()
+const { getRandomNumber } = require('../controllers/randomNumberController')
+
+// Chain the GET and SET methods to one route since both of their endpoints are similar i.e. (/)
+router.route('/').get(getRandomNumber).post(setRandomNumber)
+
+// Chain the UPDATE and DELETE methods to one route since both of their endpoints are similar i.e. (/:id)
+router.route('/:id').put(updateRandomNumber).delete(deleteRandomNumber)
+
+module.exports = router

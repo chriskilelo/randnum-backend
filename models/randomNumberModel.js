3c351ebad3b1f9c0d4a93683f60c209e16c17236
chriskilelo <chriskilelo@gmail.com>
2023-07-12 16:37:49 +0300

    Finished implemeting the RandomNumber model.

--- models/randomNumberModel.js
+++ models/randomNumberModel.js
@@ -7,7 +7,21 @@
 // Require mongoose for Object Data Modeling (ODM) in MongoDB
 const mongoose = require('mongoose')
 // Create the Random Number schema
-const randomNumberSchema = mongoose.Schema()
+const randomNumberSchema = mongoose.Schema(
+    {
+        random_number:{
+            type:String,
+            required: [true, 'The random number field cannot be empty.']
+        },
+        generated_by:{
+            type:String,
+            required:[true, 'Please provide the username of the person generating the random number.']
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
 /**
  * Export the functions and values in this module to allow 
  * for them to be accessed and used in other parts of this application

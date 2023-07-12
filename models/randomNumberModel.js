dea6f5181cf6840d5193d19e910362a63db735af
chriskilelo <chriskilelo@gmail.com>
2023-07-12 16:22:32 +0300

    Implemented the structure of the RandomNumber model.

--- models/randomNumberModel.js
+++ models/randomNumberModel.js
@@ -3,3 +3,13 @@
  * 12-JUL-2023
  * This is the model for Random Number generation
  */
+
+// Require mongoose for Object Data Modeling (ODM) in MongoDB
+const mongoose = require('mongoose')
+// Create the Random Number schema
+const randomNumberSchema = mongoose.Schema()
+/**
+ * Export the functions and values in this module to allow 
+ * for them to be accessed and used in other parts of this application
+ */
+module.exports = mongoose.model('RandomNumber', randomNumberSchema)
